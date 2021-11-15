10853a805079745a0afa2dbd1a826ca398b85217
seto1 <30764014+seto1@users.noreply.github.com>
2021-11-15 20:32:59 +0900

    DBの暗号化は動作が不安定になるため一度無効化

--- rails/app/controllers/posts_controller.rb
+++ rails/app/controllers/posts_controller.rb
@@ -3,23 +3,23 @@ class PostsController < ActionController::API
   after_action :after_action
 
   def before_action
-    key = header_key
-    begin
-      jwt_decode = JWT.decode(key, Rails.application.secrets.secret_key_base)
-      @jwt_data = jwt_decode.first
-
-      raise 'key error' unless jwt_decode.first['key']
-
-      db_path = Rails.configuration.database_configuration[ENV['RAILS_ENV']]['database']
-      decrypt = EncryptFileService.decrypt(
-        source: File.expand_path('db/enc'),
-        dest: File.expand_path(db_path),
-        key: jwt_decode.first['key']
-      )
-      raise 'db decrypt error' unless decrypt
-    rescue => e
-      return render json: { error: e }
-    end
+    # key = header_key
+    # begin
+    #   jwt_decode = JWT.decode(key, Rails.application.secrets.secret_key_base)
+    #   @jwt_data = jwt_decode.first
+    #
+    #   raise 'key error' unless jwt_decode.first['key']
+    #
+    #   db_path = Rails.configuration.database_configuration[ENV['RAILS_ENV']]['database']
+    #   decrypt = EncryptFileService.decrypt(
+    #     source: File.expand_path('db/enc'),
+    #     dest: File.expand_path(db_path),
+    #     key: jwt_decode.first['key']
+    #   )
+    #   raise 'db decrypt error' unless decrypt
+    # rescue => e
+    #   return render json: { error: e }
+    # end
   end
 
   def header_key
@@ -30,23 +30,26 @@ class PostsController < ActionController::API
   end
 
   def after_action
-    db_path = Rails.configuration.database_configuration[ENV['RAILS_ENV']]['database']
-    EncryptFileService.encrypt(
-      key: @jwt_data['key'],
-      source: File.expand_path(db_path),
-      dest: File.expand_path('db/enc')
-    )
-    EncryptFileService.delete(File.expand_path(db_path))
+    # db_path = Rails.configuration.database_configuration[ENV['RAILS_ENV']]['database']
+    # EncryptFileService.encrypt(
+    #   key: @jwt_data['key'],
+    #   source: File.expand_path(db_path),
+    #   dest: File.expand_path('db/enc')
+    # )
+    # EncryptFileService.delete(File.expand_path(db_path))
   end
 
   def index
-    render json: { success: true }
+    posts = Post.all.order(id: 'DESC')
+    render json: { success: true, posts: posts }
   end
 
   def create
+    json_request = JSON.parse(request.body.read)
+
     now = Time.current
     puts now.strftime('%Y-%m-%d %H:%M:%S')
-    post = Post.new(text: params[:text], date: now.strftime('%Y-%m-%d %H:%M:%S'))
+    post = Post.new(text: json_request['text'], date: now.strftime('%Y-%m-%d %H:%M:%S'))
 
     if post.save
       render json: { success: true }
@@ -56,13 +59,15 @@ class PostsController < ActionController::API
   end
 
   def update
+    json_request = JSON.parse(request.body.read)
+
     begin
-      post = Post.find(params[:id])
+      post = Post.find(json_request['id'])
     rescue => e
       return render json: { error: e }
     end
 
-    if post.update(text: params[:text])
+    if post.update(text: json_request['text'])
       render json: { success: true }
     else
       render json: { error: 'failed to update', validate: post.errors.messages }
@@ -70,8 +75,10 @@ class PostsController < ActionController::API
   end
 
   def destroy
+    json_request = JSON.parse(request.body.read)
+
     begin
-      post = Post.find(params[:id])
+      post = Post.find(json_request['id'])
     rescue => e
       return render json: { error: e }
     end
--- rails/app/controllers/token_controller.rb
+++ rails/app/controllers/token_controller.rb
@@ -2,19 +2,20 @@ class TokenController < ApplicationController
   skip_forgery_protection
 
   def jwt
-    begin
-      json_request = JSON.parse(request.body.read)
-      raise 'key error' unless EncryptFileService.check_key(
-        path: File.expand_path('db/enc'),
-        key: json_request['key']
-      )
-      exp = ENV['TOKEN_EXPIRE'].to_i.public_send(ENV['TOKEN_EXPIRE_UNIT']).from_now.to_i
-      preload = { key: json_request['key'], exp: exp }
-      token = JWT.encode(preload, Rails.application.secrets.secret_key_base)
-      render json: { jwt: token }
-    rescue => e
-      render json: { error: e }
-    end
+    render json: { jwt: 'test' }
+    # begin
+    #   json_request = JSON.parse(request.body.read)
+    #   raise 'key error' unless EncryptFileService.check_key(
+    #     path: File.expand_path('db/enc'),
+    #     key: json_request['key']
+    #   )
+    #   exp = ENV['TOKEN_EXPIRE'].to_i.public_send(ENV['TOKEN_EXPIRE_UNIT']).from_now.to_i
+    #   preload = { key: json_request['key'], exp: exp }
+    #   token = JWT.encode(preload, Rails.application.secrets.secret_key_base)
+    #   render json: { jwt: token }
+    # rescue => e
+    #   render json: { error: e }
+    # end
   end
 
   def test
